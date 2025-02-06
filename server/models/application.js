bdb39fefcc312039cc7aa65705c708aa16d494f0
adk-saugat <adhikarisaugat34@gmail.com>
2025-02-06 17:19:33 -0600

    creating and getting user application end-points

--- server/models/application.js
+++ server/models/application.js
@@ -1,32 +1,36 @@
 import mongoose from "mongoose"
 
-const applicationSchema = new mongoose.Schema(
-  {
-    companyName: {
-      type: "String",
-      required: true,
-      trim: true,
-    },
-    position: {
-      type: "String",
-      required: true,
-      trim: true,
-    },
-    status: {
-      type: "String",
-      trim: true,
-      enum: ["applied", "in progress", "rejected", "accepted"],
-      default: "applied",
-    },
-    notes: {
-      type: "String",
-      trim: true,
-    },
+const applicationSchema = new mongoose.Schema({
+  companyName: {
+    type: "String",
+    required: true,
+    trim: true,
   },
-  {
-    timestamps: true,
-  }
-)
+  position: {
+    type: "String",
+    required: true,
+    trim: true,
+  },
+  status: {
+    type: "String",
+    trim: true,
+    enum: ["applied", "in progress", "rejected", "accepted"],
+    default: "applied",
+  },
+  appliedDate: {
+    type: Date,
+    default: Date.now,
+  },
+  notes: {
+    type: "String",
+    trim: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+})
 
 const Application = mongoose.model("Application", applicationSchema)
 
--- server/routes/application.js
+++ server/routes/application.js
@@ -1,11 +1,33 @@
-import "../models/application.js"
 import express from "express"
 import { auth } from "../middleware/auth.js"
+import { Application } from "../models/application.js"
 
 const appRouter = express.Router()
 
-appRouter.get("/", auth, (req, res) => {
-  res.send("App router working")
+// Create an application
+appRouter.post("/create", auth, async (req, res) => {
+  try {
+    const application = new Application({ ...req.body, user: req.user._id })
+    await application.save()
+
+    await Application.findOne({
+      _id: req.user._id,
+    }).populate("User")
+
+    res.send(application)
+  } catch (error) {
+    res.status(400).send({ error })
+  }
+})
+
+// Get All User Applications
+appRouter.get("/", auth, async (req, res) => {
+  try {
+    const applications = await Application.find({ user: req.user._id })
+    res.send(applications)
+  } catch (error) {
+    res.status(400).send({ error })
+  }
 })
 
 export { appRouter }
